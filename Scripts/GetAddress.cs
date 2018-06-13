7f0cb4f53338c03b2f0b3fece0764e790949c390
Chase Slattery <chs6298@rit.edu>
2018-06-13 12:00:03 -0400

    Script Commit

---
+++ Scripts/GetAddress.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.IO;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GetAddress : NetworkManager {
+
+    // Use this for initialization
+    public GameObject serverManager;
+	void Start () {
+        
+        string address = "";
+        WebRequest request = WebRequest.Create("http://checkip.dyndns.org/");
+        using (WebResponse response = request.GetResponse())
+        using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+        {
+            address = stream.ReadToEnd();
+        }
+
+        int first = address.IndexOf("Address: ") + 9;
+        int last = address.LastIndexOf("</body>");
+        address = address.Substring(first, last - first);
+        Debug.Log(address);
+        // serverManager.GetComponent<NetworkManager>().networkAddress = address;
+        
+
+    }
+
+    public override void OnStartHost()
+    {
+        //base.OnStartHost();
+        string address = "";
+        WebRequest request = WebRequest.Create("http://checkip.dyndns.org/");
+        using (WebResponse response = request.GetResponse())
+        using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+        {
+            address = stream.ReadToEnd();
+        }
+
+        int first = address.IndexOf("Address: ") + 9;
+        int last = address.LastIndexOf("</body>");
+        address = address.Substring(first, last - first);
+        Debug.Log(address);
+
+        networkAddress = address;
+
+    }
+    public override void OnStartClient(NetworkClient client)
+    {
+        
+        string address = "";
+        WebRequest request = WebRequest.Create("http://checkip.dyndns.org/");
+        using (WebResponse response = request.GetResponse())
+        using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+        {
+            address = stream.ReadToEnd();
+        }
+
+        int first = address.IndexOf("Address: ") + 9;
+        int last = address.LastIndexOf("</body>");
+        address = address.Substring(first, last - first);
+        Debug.Log(address);
+
+        networkAddress = address;
+        
+    }
+    
+}
---
+++ Scripts/PlayerShooting.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerShooting : NetworkBehaviour
+{
+    [SerializeField] float shotCooldown = .3f;
+    [SerializeField] Transform firePosition;
+    [SerializeField] ShotEffectsmanager shotEffects;
+
+    float elapsedTime;
+    bool canShoot;
+
+    void Start()
+    {
+        shotEffects.Initialize();
+
+        if (isLocalPlayer)
+        {
+            canShoot = true;
+        }
+
+    }
+    void Update()
+    {
+        if (!canShoot)
+        {
+            return;
+        }
+
+        elapsedTime += Time.deltaTime;
+        if (Input.GetMouseButtonDown(0) && elapsedTime > shotCooldown)
+        {
+            elapsedTime = 0;
+
+            //command
+            CmdFireshot(firePosition.position, firePosition.forward);
+        }
+        
+    }
+    [Command]
+    void CmdFireshot(Vector3 origin, Vector3 direction)
+    {
+        RaycastHit hit;
+
+        Ray ray = new Ray(origin, direction);
+        Debug.DrawRay(ray.origin, ray.direction * 3f, Color.green, 1f);
+
+        //50 represents distance, its arbitrary
+        bool result = Physics.Raycast(ray, out hit, 50f);
+
+        if (result)
+        {
+            //health Magic Later
+        }
+        RpcProcessShotEffects(result, hit.point);
+
+    }
+    [ClientRpc]
+    void RpcProcessShotEffects(bool result, Vector3 point)
+    {
+        shotEffects.PlayShotEffects();
+        if (result)
+        {
+            shotEffects.PlayImpactEffect(point);
+        }
+    }
+
+}
